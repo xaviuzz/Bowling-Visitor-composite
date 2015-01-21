d9781bf3c36b49db0d7b415dcdc67d7e1f766ce8
xaviuzz <xaviuzz@gmail.com>
2015-01-21 19:05:16 +0100

    released using encapsulation of Frame

--- game.rb
+++ game.rb
@@ -1,17 +1,11 @@
 class Game
 
-  STRIKE = 10
-  NO_TRY = 0
-  TRIES_PER_FRAME = 2 
-  
   def initialize
-    reset_tries
     init_frames
   end
   
   def roll pins_down
-    increment_tries
-    anotate pins_down
+    @current_frame.anotate pins_down
     next_frame_when_needed      
   end
 
@@ -25,7 +19,7 @@ class Game
 
   def bonuses
     result = 0
-    result = @second_frame.score if is_strike(@first_frame.score)
+    result = @second_frame.score if @first_frame.strike?
     result
   end
 
@@ -34,50 +28,56 @@ class Game
   end
 
   def next_frame_when_needed
-    if (is_strike(@current_frame.score) || is_full(@current_frame.score))
+    if (@current_frame.strike? || @current_frame.full?)
       @current_frame = @second_frame
     end         
   end
 
-  def anotate pins_down
-    @current_frame.score += pins_down
-  end
-
-  def increment_tries
-    @tries += 1
-  end
-
+  
   def init_frames
     @first_frame=Frame.new
     @second_frame=Frame.new
     @current_frame = @first_frame
   end
 
-  def reset_tries
-    @tries = NO_TRY 
-  end
-
-  def is_full frame
-    return false if @tries < TRIES_PER_FRAME
-    reset_tries
-    return true
-  end
-
-  def is_strike frame
-    frame == STRIKE
-  end
+  
+  
 
 
 end
 
 class Frame
 
-  attr_accessor :score
   
-  INITIAL_FRAME_SCORE = 0
+  NO_PINS = 0
+  MAX_PINS = 10
+  NO_TRY = 0
+  TRIES_PER_FRAME = 2 
 
-  def initialize
-    @score=INITIAL_FRAME_SCORE
+  def score
+    @pins_down
   end
 
+  def initialize
+    @pins_down=NO_PINS
+    @tries= NO_TRY
+  end
+
+  def strike? 
+    @pins_down == MAX_PINS
+  end
+
+  def anotate pins_down
+    increment_tries
+    @pins_down+=pins_down
+  end
+
+  def full?
+    @tries >= TRIES_PER_FRAME
+  end
+  
+  private
+  def increment_tries
+    @tries += 1
+  end
 end
