a4f24c270a675a02f24077aef37a24272c4325e8
xaviuzz <xaviuzz@gmail.com>
2015-01-21 18:14:22 +0100

    first point of tension

--- game.rb
+++ game.rb
@@ -1,3 +1,83 @@
 class Game
+
+  STRIKE = 10
+  NO_TRY = 0
+  TRIES_PER_FRAME = 2 
   
+  def initialize
+    reset_tries
+    init_frames
+  end
+  
+  def roll pins_down
+    increment_tries
+    anotate pins_down
+    next_frame_when_needed      
+  end
+
+  def score
+    result = sum_frame_scores
+    result += bonuses
+    result
+  end
+  
+  private
+
+  def bonuses
+    result = 0
+    result = @second_frame.score if is_strike(@first_frame.score)
+    result
+  end
+
+  def sum_frame_scores
+    @first_frame.score + @second_frame.score
+  end
+
+  def next_frame_when_needed
+    if (is_strike(@current_frame.score) || is_full(@current_frame.score))
+      @current_frame = @second_frame
+    end         
+  end
+
+  def anotate pins_down
+    @current_frame.score += pins_down
+  end
+
+  def increment_tries
+    @tries += 1
+  end
+
+  def init_frames
+    @first_frame=Frame.new
+    @second_frame=Frame.new
+    @current_frame = @first_frame
+  end
+
+  def reset_tries
+    @tries = NO_TRY 
+  end
+
+  def is_full frame
+    return false if @tries < TRIES_PER_FRAME
+    reset_tries
+    return true
+  end
+
+  def is_strike frame
+    frame == STRIKE
+  end
+
+
+end
+
+class Frame
+
+  attr_accessor :score
+  
+  INITIAL_FRAME_SCORE = 0
+
+  def initialize
+    @score=INITIAL_FRAME_SCORE
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,11 +2,46 @@ require './game'
 
 describe "The Bowling Game" do
 
+  @game
+  ANY_ROLL=3
+  STRIKE =10
 
-  it "scores pins " do
-
-  
+  before do
+    @game=Game.new
   end
 
+  it "scores pins " do
+    @game.roll(ANY_ROLL)
+    expect(@game.score()).to eq(ANY_ROLL)
+  end
+
+  it "scores the second try too " do
+    number_of_rolls = 2
+    @game.roll(ANY_ROLL)
+    @game.roll(ANY_ROLL)
+    expect(@game.score()).to eq(ANY_ROLL*number_of_rolls)
+  end
+
+  it "scores a spare" do
+    @game.roll(ANY_ROLL)
+    @game.roll(STRIKE-ANY_ROLL)
+    @game.roll(ANY_ROLL)
+    expect(@game.score()).to eq(16)
+  end
+
+  it "scores a strike" do
+    @game.roll(STRIKE)
+    @game.roll(ANY_ROLL)
+    @game.roll(ANY_ROLL)
+    expect(@game.score()).to eq(22)
+  end
+
+  it "scores the next frame" do
+    @game.roll(4)
+    @game.roll(ANY_ROLL)
+    @game.roll(ANY_ROLL)
+    @game.roll(ANY_ROLL)
+    expect(@game.score()).to eq(13)
+  end
 
 end
